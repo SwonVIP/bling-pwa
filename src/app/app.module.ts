22ab856749dcd418b6f49662b82eeeef9870328b
Sven.Scheffel <sven.scheffel@de.ebmpapst.com>
2018-10-02 10:24:32 +0200

    * Added basic routing skeleton * Added welcome plain component * Added home plain component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { HomeComponent } from './pages/home/home.component';
+import {AppRoutingModule} from './navigation/app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
